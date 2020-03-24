a22f4825e3926cd8df513babc8a88c70b5f9b3db
= <ashillpathak@gmail.com>
2020-03-24 10:57:33 +0000

    runner

---
+++ Runner.java
@@ -0,0 +1,14 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Tax test = new Tax();
+		System.out.println(test.userName("Tee"));
+		test.Salary(100000);
+		System.out.println(test.Tax_Category());
+		
+
+	}
+
+}
