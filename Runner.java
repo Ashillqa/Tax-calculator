569ac35e5006fe5a56a39f9794fdfd7bc4e66323
Ashillqa <62261869+Ashillqa@users.noreply.github.com>
2020-03-24 11:06:35 +0000

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -4,9 +4,9 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Tax test = new Tax();
-		System.out.println(test.userName("Tee"));
+		test.userName("Tee");
 		test.Salary(100000);
-		System.out.println(test.Tax_Category());
+		test.Tax_Category();
 		
 
 	}
